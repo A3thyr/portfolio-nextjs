56d834b21b1abf2c39d30fb48e17492a1179a605
A3thyr <a3therys@gmail.com>
2024-05-13 20:59:47 +0300

    feat: added animation to skills component

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,12 +1,37 @@
 import { skillsData } from "@/lib/data";
+import { motion } from "framer-motion";
+
+const fadeInAnimationVariants = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05 * index,
+    },
+  }),
+};
 
 export default function Skills() {
   return (
     <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
       {skillsData.map((skill, index) => (
-        <li className="bg-white border rounded-xl px-5 py-3 " key={index}>
+        <motion.li
+          className="bg-white border rounded-xl px-5 py-3 "
+          key={index}
+          variants={fadeInAnimationVariants}
+          initial="initial"
+          whileInView="animate"
+          viewport={{
+            once: true,
+          }}
+          custom={index}
+        >
           {skill}
-        </li>
+        </motion.li>
       ))}
     </ul>
   );
